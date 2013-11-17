856da05ab8077161488526d5ed2aed8084307c10
Olav Emil Eiksund <olavemil@stud.ntnu.no>
2013-11-17 17:43:33 +0100

    Updated assembly

---
+++ delay.s
@@ -0,0 +1,26 @@
+﻿;delay
+lda r2, [r0];load sample to r2 from a mem
+ldi #255	;load 255
+mov r7, r1	;move 255 to r7
+
+ldi #coeff	;load coeff  fraction dry
+mov r4, r1	;move coeff to r4
+
+
+ldi #delaytime	;load delaytime
+mov r8, r1	;move delaytime to r8
+lda r3, [r8];load sample to r3 from a mem
+
+mr1 r2, r4	;r2 *= r4
+
+ldi #1		;load 1
+mov r5, r1	;move 1 to r5
+sbi r5, r4	;r5 -= r4    fraction wet
+
+mr1 r3, r5	;r3 *= r4
+adi r2, r3	;r2 += r3
+
+cmp r7, r2	;check r2 larger than 255
+blt =17
+mov r2, r7	;r2 = 255
+stb r2, [r0];store r2
---
+++ sdft.s
@@ -0,0 +1,29 @@
+﻿;isdft
+;init code
+ldi #255	  ;load n-1
+mov r6, r1
+ldi #6144 ;1/n  1/256 = 0.00110.0000000000 = #6144; 1/512 = 0.00111.0000000000 = #6400
+mov r7, r1
+
+;sample loop
+mov r4, r0
+mov r5, r0
+
+mov r9, r0 ; initiar1ze loop counter to zer0
+loop:
+	lda r2, [r9] ;load f[r9] and f[r9+1] into r2, and r3
+	ldc [r9]	  ;load c[r9] and c[r9+1] into c, and d
+		
+	msc r2, r3 ;r2 -= r3*c
+	msd r3, r2 ;r3 -= r2*d
+	msc r2, r3 ;r2 -= r3*c
+
+	adf r4, r2 ;r4 += r2
+
+	cmp r9, r6 ;compare r9 to n-1
+blt =7
+mlf r4, r7 ;r4 *= 1/n
+f2i r4, r4 ;cast to int
+stb [r0], r4 ;store to mem
+
+halt ;sleep
